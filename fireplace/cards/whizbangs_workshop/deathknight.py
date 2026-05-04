feed66618afdc1d3aa16fe8d370c873b2a960ff9
ZJUxjy <948062294@qq.com>
2026-05-05 07:14:43 +0800

    feat(whizbang): add death knight spells and weapon

--- fireplace/cards/whizbangs_workshop/deathknight.py
+++ fireplace/cards/whizbangs_workshop/deathknight.py
@@ -75,6 +75,56 @@ class TOY_828e:
 # Spells
 
 
+class TOY_825:
+    """Lesser Spinel Spellstone"""
+
+    play = Buff(FRIENDLY_HAND + UNDEAD, "TOY_825e")
+    progress_total = 5
+    reward = Morph(SELF, "TOY_825t")
+
+    class Hand:
+        events = Death(FRIENDLY + MINION).on(AddProgress(SELF, Death.ENTITY))
+
+
+class TOY_825t:
+    """Spinel Spellstone"""
+
+    play = Buff(FRIENDLY_HAND + UNDEAD, "TOY_825e2")
+    progress_total = 5
+    reward = Morph(SELF, "TOY_825t2")
+
+    class Hand:
+        events = Death(FRIENDLY + MINION).on(AddProgress(SELF, Death.ENTITY))
+
+
+class TOY_825t2:
+    """Greater Spinel Spellstone"""
+
+    play = Buff(FRIENDLY_HAND + UNDEAD, "TOY_825e3")
+
+
+TOY_825e = buff(+1, +1)
+TOY_825e2 = buff(+2, +2)
+TOY_825e3 = buff(+3, +3)
+
+
+class TOY_826:
+    """Threads of Despair"""
+
+    play = Buff(ALL_MINIONS, "TOY_826e")
+
+
+@custom_card
+class TOY_826e:
+    tags = {
+        GameTag.CARDNAME: "Threads of Despair",
+        GameTag.CARDTYPE: CardType.ENCHANTMENT,
+        GameTag.DEATHRATTLE: True,
+    }
+
+    deathrattle = Hit(ALL_MINIONS, 1)
+
+
 class MIS_100:
     """Helm of Humiliation"""
 
@@ -102,3 +152,29 @@ class MIS_100e2:
         GameTag.CARDNAME: "Inspired",
         GameTag.CARDTYPE: CardType.ENCHANTMENT,
     }
+
+
+##
+# Weapons
+
+
+class MIS_101_GainDurability(TargetedAction):
+    TARGET = ActionArg()
+
+    def do(self, source, weapon):
+        player = weapon.controller
+        if getattr(player, "corpses", 0) < 3:
+            return
+        player.corpses -= 3
+        return source.game.queue_actions(source, [
+            Buff(weapon, "MIS_101e")
+        ])
+
+
+class MIS_101:
+    """Foamrender"""
+
+    events = Attack(FRIENDLY_HERO).on(MIS_101_GainDurability(SELF))
+
+
+MIS_101e = buff(health=1)
--- tests/test_whizbangs_deathknight.py
+++ tests/test_whizbangs_deathknight.py
@@ -98,3 +98,132 @@ def test_darkthorn_quilter_splits_attack_damage_among_enemies_at_turn_end():
         minion.health for minion in player.opponent.field
     )
     assert total_health_after == total_health_before - quilter.atk
+
+
+def test_lesser_spinel_spellstone_buffs_undead_in_hand():
+    """Lesser Spinel Spellstone gives Undead in hand +1/+1."""
+    game = prepare_empty_game(CardClass.DEATHKNIGHT, CardClass.DEATHKNIGHT)
+    player = game.current_player
+    player.max_mana = 10
+    player.used_mana = 0
+    undead = player.give("ICC_026")
+    non_undead = player.give("CS2_182")
+
+    player.give("TOY_825").play()
+
+    assert (undead.atk, undead.health) == (undead.data.atk + 1, undead.data.health + 1)
+    assert (non_undead.atk, non_undead.health) == (
+        non_undead.data.atk,
+        non_undead.data.health,
+    )
+
+
+def test_spinel_spellstone_buffs_undead_in_hand_by_2():
+    """Spinel Spellstone gives Undead in hand +2/+2."""
+    game = prepare_empty_game(CardClass.DEATHKNIGHT, CardClass.DEATHKNIGHT)
+    player = game.current_player
+    player.max_mana = 10
+    player.used_mana = 0
+    undead = player.give("ICC_026")
+
+    player.give("TOY_825t").play()
+
+    assert (undead.atk, undead.health) == (undead.data.atk + 2, undead.data.health + 2)
+
+
+def test_greater_spinel_spellstone_buffs_undead_in_hand_by_3():
+    """Greater Spinel Spellstone gives Undead in hand +3/+3."""
+    game = prepare_empty_game(CardClass.DEATHKNIGHT, CardClass.DEATHKNIGHT)
+    player = game.current_player
+    player.max_mana = 10
+    player.used_mana = 0
+    undead = player.give("ICC_026")
+
+    player.give("TOY_825t2").play()
+
+    assert (undead.atk, undead.health) == (undead.data.atk + 3, undead.data.health + 3)
+
+
+def test_lesser_spinel_spellstone_upgrades_after_five_friendly_minions_die():
+    """Lesser Spinel Spellstone upgrades after its controller gains 5 Corpses."""
+    game = prepare_empty_game(CardClass.DEATHKNIGHT, CardClass.DEATHKNIGHT)
+    player = game.current_player
+    player.max_mana = 10
+    player.used_mana = 0
+    player.give("TOY_825")
+
+    for _ in range(5):
+        minion = player.summon(WISP)
+        player.give(MOONFIRE).play(target=minion)
+
+    assert "TOY_825" not in [card.id for card in player.hand]
+    assert "TOY_825t" in [card.id for card in player.hand]
+
+
+def test_spinel_spellstone_upgrades_to_greater_after_five_more_deaths():
+    """Spinel Spellstone upgrades to Greater after 5 more Corpses."""
+    game = prepare_empty_game(CardClass.DEATHKNIGHT, CardClass.DEATHKNIGHT)
+    player = game.current_player
+    player.max_mana = 10
+    player.used_mana = 0
+    player.give("TOY_825t")
+
+    for _ in range(5):
+        minion = player.summon(WISP)
+        player.give(MOONFIRE).play(target=minion)
+
+    assert "TOY_825t" not in [card.id for card in player.hand]
+    assert "TOY_825t2" in [card.id for card in player.hand]
+
+
+def test_threads_of_despair_gives_all_current_minions_deathrattle():
+    """Threads of Despair gives all current minions a board-damage Deathrattle."""
+    game = prepare_empty_game(CardClass.DEATHKNIGHT, CardClass.DEATHKNIGHT)
+    player = game.current_player
+    player.max_mana = 10
+    player.used_mana = 0
+    doomed = player.summon(WISP)
+    friendly_survivor = player.summon("EX1_572")
+    enemy_survivor = player.opponent.summon("EX1_572")
+    late_minion = None
+
+    player.give("TOY_826").play()
+    late_minion = player.summon("EX1_572")
+
+    player.give(MOONFIRE).play(target=doomed)
+
+    assert doomed.dead
+    assert friendly_survivor.health == friendly_survivor.data.health - 1
+    assert enemy_survivor.health == enemy_survivor.data.health - 1
+    assert late_minion.health == late_minion.data.health - 1
+
+
+def test_foamrender_spends_corpses_to_gain_durability_on_hero_attack():
+    """Foamrender spends 3 Corpses to gain Durability whenever the hero attacks."""
+    game = prepare_empty_game(CardClass.DEATHKNIGHT, CardClass.DEATHKNIGHT)
+    player = game.current_player
+    player.max_mana = 10
+    player.used_mana = 0
+    player.corpses = 3
+    weapon = player.give("MIS_101").play()
+
+    player.hero.attack(player.opponent.hero)
+
+    assert player.corpses == 0
+    assert player.weapon is weapon
+    assert weapon.durability == 1
+
+
+def test_foamrender_does_not_gain_durability_without_corpses():
+    """Foamrender does not gain Durability unless 3 Corpses can be spent."""
+    game = prepare_empty_game(CardClass.DEATHKNIGHT, CardClass.DEATHKNIGHT)
+    player = game.current_player
+    player.max_mana = 10
+    player.used_mana = 0
+    player.corpses = 2
+
+    player.give("MIS_101").play()
+    player.hero.attack(player.opponent.hero)
+
+    assert player.corpses == 2
+    assert player.weapon is None
